32218a4b54a74640648d55cbd3cf366c5a26b940
Isaac Tsang <ndtm.idea@gmail.com>
2019-01-29 17:21:31 +0800

    jsonista support

--- src/main/clojure/sweet/json.clj
+++ src/main/clojure/sweet/json.clj
@@ -1,11 +1,15 @@
 (ns sweet.json
-  (:refer-clojure :exclude [read]))
+  (:refer-clojure :exclude [read])
+  (:require [clojure.java.io :as io]))
 
 
-(declare read write read-str write-str)
+(declare read write read-str write-str read-bytes write-bytes)
 
 
 (cond-require
+ 'jsonista.core
+ (load "jsonista_core")
+
  'cheshire.core
  (load "cheshire_core")
 
@@ -14,3 +18,14 @@
 
  :else
  (throw "cheshire.core OR clojure.data.json required"))
+
+
+(when-not (bound? #'read-bytes)
+  (defn read-bytes [^bytes bs & opts]
+    (apply read (io/reader bs) opts)))
+
+(when-not (bound? #'write-bytes)
+  (defn write-bytes ^bytes [x & opts]
+    (let [out (ByteArrayOutputStream.)]
+      (apply write x (io/writer out) opts)
+      (.toByteArray out))))
---
+++ src/main/clojure/sweet/jsonista_core.clj
@@ -0,0 +1,13 @@
+(in-ns 'sweet.json)
+
+
+(require '[jsonista.core :as jsonista])
+
+
+(defalias read        jsonista/read-value)
+(defalias read-str    jsonista/read-value)
+(defalias read-bytes  jsonista/read-value)
+
+(defalias write       jsonista/write-value)
+(defalias write-str   jsonista/write-value-as-string)
+(defalias write-bytes jsonista/write-value-as-bytes)
