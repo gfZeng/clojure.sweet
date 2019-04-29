913ef6ec792c157cac5662a84c57203d4cf63420
Isaac Tsang <ndtm.idea@gmail.com>
2019-04-30 01:33:38 +0800

    transit support

---
+++ src/main/clojure/sweet/data/transit.clj
@@ -0,0 +1,28 @@
+(ns sweet.data.transit
+  (:refer-clojure :exclude [read])
+  (:require [cognitect.transit :as transit])
+  (:import [java.io ByteArrayOutputStream ByteArrayInputStream]))
+
+
+
+(defalias cognitect.transit)
+
+
+(defn write-bytes [x]
+  (with-open [out (ByteArrayOutputStream. 4096)]
+    (let [wrt (transit/writer out :json)]
+      (write wrt x)
+      (.toByteArray out))))
+
+(defn write-str [x]
+  (String. (write-bytes x)))
+
+(defn read-bytes [bs]
+  (with-open [in (ByteArrayInputStream. bs)]
+    (let [rdr (transit/reader in :json)]
+      (read rdr))))
+
+(defn read-str [s]
+  (read-bytes (.getBytes s)))
+
+
