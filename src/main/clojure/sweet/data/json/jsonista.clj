84fc970c30b4a59a20d1c5c6980db4f081e67c45
Isaac Tsang <ndtm.idea@gmail.com>
2019-05-04 11:38:14 +0800

    disable read-bytes & write-bytes for jsonista

--- src/main/clojure/sweet/data/json/jsonista.clj
+++ src/main/clojure/sweet/data/json/jsonista.clj
@@ -6,11 +6,11 @@
 
 (defalias read        jsonista/read-value)
 (defalias read-str    jsonista/read-value)
-(defalias read-bytes  jsonista/read-value)
+; (defalias read-bytes  jsonista/read-value)
 
 (defalias write       jsonista/write-value)
 (defalias write-str   jsonista/write-value-as-string)
-(defalias write-bytes jsonista/write-value-as-bytes)
+; (defalias write-bytes jsonista/write-value-as-bytes)
 
 (defn mapper [opts]
   (jsonista/object-mapper opts))
